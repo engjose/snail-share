2b6129221d1b69d9b51a22aff3b268248d93a9ba
changsunhaipeng <zhangsun.haipeng@puscene.com>
2017-10-14 17:47:49 +0800

    splash

---
+++ app/src/main/java/com/snail/snailshare/splash/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.snail.snailshare.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.snail.snailshare.R;
+import com.snail.snailshare.main.MainActivity;
+
+import me.wangyuwei.particleview.ParticleView;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    private ParticleView mParticleView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        initView();
+    }
+
+    private void initView() {
+        mParticleView = (ParticleView) findViewById(R.id.particle_view);
+        mParticleView.startAnim();
+        mParticleView.setOnParticleAnimListener(new ParticleView.ParticleAnimListener() {
+            @Override
+            public void onAnimationEnd() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
